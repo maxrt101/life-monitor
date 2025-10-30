9970b119fea6ef7250f7aff80b32c0745462a937
maxrt <max.r.tkachuk@gmail.com>
2025-10-31 01:49:22 +0200

    Add i2c command for detecting i2c devices on the bus

---
+++ src/cli/sh_cmd_i2c.c
@@ -0,0 +1,39 @@
+/** ========================================================================= *
+*
+ * @file sh_cmd_i2c.c
+ * @date 31-10-2025
+ * @author Maksym Tkachuk <max.r.tkachuk@gmail.com>
+ *
+ * @brief 'i2c' CLI Command implementation
+ *
+ *  ========================================================================= */
+
+/* Includes ================================================================= */
+#include <project.h>
+
+#include "shell/shell.h"
+#include "log/log.h"
+#include "bsp.h"
+#include <string.h>
+
+/* Defines ================================================================== */
+#define LOG_TAG shell
+
+/* Macros =================================================================== */
+/* Exposed macros =========================================================== */
+/* Enums ==================================================================== */
+/* Types ==================================================================== */
+/* Variables ================================================================ */
+/* Private functions ======================================================== */
+/* Shared functions ========================================================= */
+static int8_t cmd_i2c(shell_t * sh, uint8_t argc, const char ** argv) {
+  i2c_detect_result_t detect;
+
+  i2c_detect(&device.board.i2c, detect);
+  i2c_detect_dump(detect);
+
+  return SHELL_OK;
+}
+
+SHELL_DECLARE_COMMAND(i2c, cmd_i2c, "I2C bus control");
+
